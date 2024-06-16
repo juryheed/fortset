50aeedc8443aaef28eed8a3254e19041095e8392
juryheed <rabitangl20@naver.com>
2024-06-17 05:59:42 +0900

    first commit

---
+++ src/test/java/org/mjulikelion/fortest/FortestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.fortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
